09e10f2b26db97f0b46d8fb4e7094cdb96563988
Edi Kurniawan <edikurniawan27@outlook.com>
2021-08-17 15:13:03 +0700

    basic react state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: [],
+    };
+  }
+
+  getUser = async () => {
+    let response = await axios.get(
+      'https://jsonplaceholder.typicode.com/users/1',
+    );
+    this.setState({
+      user: response.data,
+    });
+  };
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  render() {
+    const user = this.state.user;
+    return (
+      <div>
+        <div>my name is {user.name}</div>
+        <div>my username is {user.username}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
