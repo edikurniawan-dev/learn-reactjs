e9c1c9d01cfa9ec036266c75b2ebaa5844f43a7f
Edi Kurniawan <edikurniawan27@outlook.com>
2021-08-21 14:38:02 +0700

    reusable component and props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
+import Card from './components/Card';
 
 function App() {
     return (
         <div className="py-4">
             <div className="container">
-                <h4>the post</h4>
+                <h4>The Post</h4>
                 <hr />
                 <div className="row">
                     <div className="col-md-4">
-                        <div className="card">
-                            <img
-                                src="http://placekitten.com/g/300/200"
-                                alt=""
-                                className="card-img-top"
-                            />
-                            <div className="card-body">
-                                <h4>the first post</h4>
-                                <small className="text-muted">
-                                    published 6 feb 2021
-                                </small>
-                            </div>
-                        </div>
+                        <Card
+                            content={{
+                                imageUrl: 'http://placekitten.com/g/300/200',
+                                title: 'First Post',
+                                published: '8 June 2021',
+                            }}
+                        />
+                    </div>
+                    <div className="col-md-4">
+                        <Card
+                            content={{
+                                imageUrl: 'http://placekitten.com/300/200',
+                                title: 'Second Post',
+                                published: '8 July 2021',
+                            }}
+                        />
+                    </div>
+                    <div className="col-md-4">
+                        <Card
+                            content={{
+                                imageUrl: 'http://placekitten.com//300/200',
+                                title: 'Third Post',
+                                published: '8 August 2021',
+                            }}
+                        />
                     </div>
                 </div>
             </div>
