c99941d4f600906b480ef1d59dee1cf38a527d34
kurniawanedi <edikurniawan27@outlook.com>
2021-08-24 11:10:12 +0700

    fetching data with useEffect

--- src/App.js
+++ src/App.js
@@ -2,24 +2,68 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 function App(props) {
-    const [users, setUsers] = useState([]);
+    const [identifier, setIdentifier] = useState(1);
+    const [user, setUser] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    const getUsers = async () => {
+    const getUser = async () => {
+        setLoading(true);
         try {
             let response = await axios.get(
-                'https://jsonplaceholder.typicode.com/users',
+                `https://jsonplaceholder.typicode.com/users/${identifier}`,
             );
-            setUsers(response.data);
+            setLoading(false);
+            setUser(response.data);
         } catch (e) {
+            setLoading(true);
             console.log(e.message);
         }
     };
 
     useEffect(() => {
-        getUsers();
-    }, []);
+        getUser();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [identifier]);
 
-    return <div>hello</div>;
+    return (
+        <div className="py-5">
+            <div className="container">
+                <div className="row">
+                    <input
+                        type="text"
+                        name="identifier"
+                        value={identifier}
+                        onChange={(e) => setIdentifier(e.target.value)}
+                        className="form-control"
+                    />
+                    {loading ? (
+                        <div>Loading . . . .</div>
+                    ) : (
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Username</th>
+                                    <th>Email</th>
+                                    <th>Website</th>
+                                    <th>Phone</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{user.name}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.website}</td>
+                                    <td>{user.phone}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
